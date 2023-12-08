8ada7cc356aeca5ef2fc3964a4b14cda67779017
choncey <nchoncey@gmail.com>
2023-12-08 11:04:05 -0800

    created bash script

--- bash_milestone.sh
+++ bash_milestone.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 #Ask the user a question
 
-	echo Do you want to go to jail, or do you want to go home?
+	echo Do you want the hook up?
 
-if [ condition = 'home' ]
+if [ condition = 'yes' ]
 then
-	echo Glad you want to go home.
+	echo Aight, I got you.
+
+# this will install apache2
+sudo apt install apache2
+
+# this will install mysql
+sudo apt install mysql-server
+
+#this will install postgres
+sudo apt install postgresql
 
 	.
 fi
